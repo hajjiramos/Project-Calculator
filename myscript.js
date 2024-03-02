35eb8094c710d1fd2b81c54e13f92bfabe74c863
Hajji Ramos <leo_pips@yahoo.com>
2024-03-02 16:30:14 -0600

    modifies the display in css

--- myscript.js
+++ myscript.js
@@ -1,26 +1,24 @@
-function add(num1,num2) {
-    return num1+num2;
-}
+//-- Functions Basic Math Operation --//
+    function add(num1,num2) {
+        return num1+num2;
+    }
 
+    function subtract(num1,num2) {
+        return num1-num2;
+    }
 
-function subtract(num1,num2) {
-    return num1-num2;
-}
+    function multiply(num1,num2) {
+        return num1*num2;
+    }
 
-
-function multiply(num1,num2) {
-    return num1*num2;
-}
-
-
-function divide(num1,num2) {
-    return num1/num2;
-}
+    function divide(num1,num2) {
+        return num1/num2;
+    }
 
 
 let operator='';
-let firstNumber='';
-let secondNumber='';
+let firstNum='';
+let secondNum='';
 
 function operate() {
 
